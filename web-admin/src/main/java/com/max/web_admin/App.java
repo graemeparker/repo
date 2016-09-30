ec32cb8d85c00a694e174477de201b5628fccde1
graeme parker <graemesparker@gmail.com>
2016-09-30 19:31:40 +0100

    Initial commit

---
+++ web-admin/src/main/java/com/max/web_admin/App.java
@@ -0,0 +1,13 @@
+package com.max.web_admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
